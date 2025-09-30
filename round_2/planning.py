71303aca9a8fc66a9296e0603aba8f4534636c3f
Jet Semrick <jet@avallon.ai>
2025-09-29 23:31:30 -0700

    check in'

---
+++ round_2/planning.py
@@ -0,0 +1,59 @@
+## Cursor 201
+'''
+Live Demo
+- Game
+- Blog
+- Algorithm from school
+
+1. Start a background agent to build out the frontend
+of an application. Set up OS notifications.
+2. 
+'''
+ 
+ # 1. Model Selection
+'''
+General Purpose
+- Claude Sonnet 4
+Better for complex system design discussions.
+- GPT-5
+Better for fast implementation and MVP development.
+
+Thinking mode
+- AI model will take extra time to work through complex problems
+step by step. Useful for complex debugging, architecture decisions, 
+advanced algorithms, and system design.
+'''
+
+ # 2. Context Management
+'''
+Cursor Rules
+- 
+'''
+
+ # 3. Custom Rules
+'''
+Cursor Rules
+- 
+
+'''
+
+ # 4. MCP Workflows
+
+ # Feature Highlights
+'''
+/summarize
+- Will summarize context on-demand, useful when you don't want to create a new chat, but want to free up space in the context window.
+
+Custom Slash Commands
+- .cursor/commands/[command].md
+
+OS Notifications from Agent
+
+GitHub Support for Background Agents
+
+Background Agents
+
+Queue Follow-Up Messages
+
+MCP Resources Support
+'''
